c9b7beda9beebc410657f33af13e29eedba9b9b9
jgfranco17 <chino.franco@gmail.com>
2023-05-25 13:18:25 +0900

    Created fixture for sample Latex input

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,7 @@
-import sys
 import pytest
+from rendertex import LatexString
 
 
-# each test runs on cwd to its temp dir
-@pytest.fixture(autouse=True)
-def go_to_tmpdir(request):
-    # Get the fixture dynamically by its name.
-    tmpdir = request.getfixturevalue("tmpdir")
-    # ensure local test created packages can be imported
-    sys.path.insert(0, str(tmpdir))
-    # Chdir only for the duration of the test.
-    with tmpdir.as_cwd():
-        yield
+@pytest.fixture
+def latex_string():
+    return LatexString(r'\frac{1}{2} \cdot \left(\sqrt{9}\right)^3')
