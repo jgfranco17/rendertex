42f92206282f7418753166dfb45968acb055dc11
jgfranco17 <chino.franco@gmail.com>
2023-05-25 13:11:41 +0900

    Updated internal documentation

--- rendertex/base.py
+++ rendertex/base.py
@@ -9,25 +9,44 @@ class LatexString:
         self.__formula = formula
         self.__image = self.latex_to_image(self.__formula)
 
+    def __repr__(self) -> str:
+        return f'<LatexString formula=\"{self.__formula}\">'
+
+    def __str__(self) -> str:
+        return f'${self.__formula}$'
+
     @staticmethod
     def latex_to_image(text: str) -> str:
+        """
+        Convert a text string into LaTeX-formatted image.
+
+        Args:
+            text (str): LaTeX string to be rendered
+
+        Returns:
+            str: File path of output image
+        """
+        # Generate LaTeX figure
         figure = plt.figure()
         plt.axis("off")
         plt.text(0.5, 0.5, f'${text}$')
 
+        # Create PDF export
         pdf_output = os.path.join(os.getcwd(), "temp.pdf")
         plt.savefig(pdf_output, format="pdf", bbox_inches="tight", pad_inches=0.3)
         plt.close(figure)
 
+        # Render as image
         image_output = os.path.join(os.getcwd(), "result.png")
         image = convert_from_path(pdf_output)[0]
         image.save(image_output, "PNG")
+        os.remove(pdf_output)
         return image_output
 
     def display(self) -> None:
         try:
             image = cv2.imread(self.__image, cv2.IMREAD_UNCHANGED)
-            cv2.imshow('Image', image)
+            cv2.imshow("LaTeX Formula", image)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
 
